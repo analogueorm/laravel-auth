46ae982a5c489c4b09892329c338ad806b88653a
Rémi Collin <remi@plateau.io>
2014-12-04 17:15:54 +0100

    Initial Commit

---
+++ src/AnalogueAuthServiceProvider.php
@@ -0,0 +1,47 @@
+<?php namespace Analogue\LaravelAuth;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\AuthManager;
+
+class AnalogueAuthServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	public function boot()
+	{
+		$this->app[AuthManager::class]->extend('analogue', function ($app) {
+			
+			return new AnalogueUserProvider(
+				$app['Illuminate\Hashing\HasherInterface'],
+				$app['analogue'],
+				$app['config']['auth.model']
+			);
+		});
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
